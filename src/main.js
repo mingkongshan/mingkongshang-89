67e03e39e6350d0293cdfceae17b4126776b1a28
unknown <xuch@xampie.com>
2019-12-21 20:42:20 +0800

    黑马头条PC-登录模块-安装axios-配置全局使用

--- src/main.js
+++ src/main.js
@@ -3,10 +3,15 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui' // 引入包
 import 'element-ui/lib/theme-chalk/index.css' // 引入elementUI样式
-import './styles/index.less' // 引入初始化样式
+import './styles/index.less' // 全局注册
+import axios from 'axios' // 引入初始化样式
+Vue.config.productionTip = false
+Vue.use(ElementUI)
+
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/captchas/:mobile'// 复制黑马头条的默认地址
+Vue.prototype.$axios = axios // 赋值给全局对象
 Vue.config.productionTip = false
 Vue.use(ElementUI) // 全局注册
-
 new Vue({
   router,
   render: h => h(App)
