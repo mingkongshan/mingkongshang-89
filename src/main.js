6516a534561d472a9ee85f9b4335ad3b7f3ba48b
unknown <xuch@xampie.com>
2019-12-28 10:08:25 +0800

    黑马头条PC-接口访问-token统一处理思考

--- src/main.js
+++ src/main.js
@@ -5,12 +5,12 @@ import router from './router'
 import ElementUI from 'element-ui' // 引入包
 import 'element-ui/lib/theme-chalk/index.css' // 引入elementUI样式
 import './styles/index.less' // 全局注册
-import axios from 'axios'
+import axios from './utils/request'
 import Component from './components' // 引入初始化样式
 Vue.config.productionTip = false
 Vue.use(ElementUI)// 引入组件对象
 Vue.use(Component) // 注册全局组件对象
-axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'// 复制黑马头条的默认地址
+
 Vue.prototype.$axios = axios // 赋值给全局对象
 
 Vue.use(ElementUI) // 全局注册
---
+++ src/utils/request.js
@@ -0,0 +1,15 @@
+// 对axion进行一个封装
+import axios from 'axios'
+// 请求拦截器
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0' // 赋值黑马头条的默认地址
+
+axios.interceptors.request.use(function (config) {
+  // 执行请求ok
+  // config 是axios的所有配置
+  let token = window.localStorage.getItem('user-token') // 获取token
+  config.headers.Authorization = `Bearer ${token}`// 统一注入token
+  return config // 返回config 就会作为新的请求选项
+}, function () {
+  // 执行请求失败
+})
+export default axios
