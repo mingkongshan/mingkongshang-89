bea23c6850fc4fba2ce09ee217365e7ed15a0b29
unknown <xuch@xampie.com>
2019-12-29 16:23:59 +0800

    黑马头条PC-评论列表-页面结构-table

--- src/components/common/bread-crumb.vue
+++ src/components/common/bread-crumb.vue
@@ -1,11 +1,11 @@
 <template>
   <!-- 面包屑的内容 -->
-  <el-breadcrumb>
-      <el-breadcrum-item to="/home">首页</el-breadcrum-item>
-      <el-breadcrum-item>
+  <el-breadcrumb separator=">">
+      <el-breadcrumb-item to="/home">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>
           <!-- 具名插槽 -->
           <slot name='title'></slot>
-      </el-breadcrum-item>
+      </el-breadcrumb-item>
   </el-breadcrumb>
 </template>
 
